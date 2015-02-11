5cebd3c8a83c0355c77ca9dd0b600c12efbe37e0
Dustin Morando <dustinmorando@me.com>
2015-02-10 20:01:22 -0500

    added backbone aplication state cove and linked it to HTML with templates

--- scripts/main.js
+++ scripts/main.js
@@ -1,30 +1,28 @@
-// var deck = [
-//   {front: "How do you find out how many elements are in an array?", back: "`.length`"},
-//   {front: "What HTML element should be used for a blog post?", back: "section"}
-// ];
-//
-// var MyRouter = Backbone.Router.extend({
-//   routes: {
-//     "": "index",
-//     "card/:id": "card"
-//   },
-//
-//   index: function(){
-//     var template = _.template( $('#card-list-template').text() );
-//     var renderedTemplate = template({cards: deck});
-//     $('.app-container').html(renderedTemplate);
-//   },
-//
-//   card: function(id){
-//     var cardIndex = Number(id);
-//     var card = deck[cardIndex];
-//
-//     var template = _.template( $('#card-template').text() );
-//     var renderedTemplate = template(card);
-//     $('.app-container').html(renderedTemplate);}
-// });
-//
-// $(document).ready(function(){
-//   var router = new MyRouter();
-//   Backbone.history.start();
-// });
+////////////////////////////////
+///Aplication State
+////////////////////////////////
+var MyRouter = Backbone.Router.extend({
+      routes: {
+        "": "index",
+        "results/:id": "results"
+      },
+      index: function() {
+        var template = _.template($('#job-and-homes-template').text());
+        var renderedTemplate = template("job-and-homes-template");
+        $('.app-container').html(renderedTemplate);
+      },
+
+      results: function(id) {
+        var resultsIndex = Number(id);
+        var results = deck[resultsIndex];
+        var template = _.template($('#results-template').text());
+        var renderedTemplate = template("results-template");
+        $('.app-container').html(renderedTemplate);
+      },
+
+    });
+
+      $(document).ready(function(){
+      var router = new MyRouter();
+      Backbone.history.start();
+    });
