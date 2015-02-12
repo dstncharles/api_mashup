ba5f65e3786014b68c1dbd6d984da32377382ed8
Dustin Morando <dustinmorando@me.com>
2015-02-11 20:07:09 -0500

    added the render function i guess?

--- scripts/main.js
+++ scripts/main.js
@@ -15,7 +15,7 @@ var ZipCollection = Backbone.Collection.extend({
 ////////////////////////////////
 ///View
 ////////////////////////////////
-var ZipCodeInputView = Backbone.View.extend({
+var ZipInputView = Backbone.View.extend({
   tagName: 'form',
   events: {
     'submit': 'createData'
@@ -31,8 +31,18 @@ var ZipCodeInputView = Backbone.View.extend({
   render: function() {
     this.$el.html(this.template());
     return this;
-  }
-});
+  })
+
+var ZipsListView = Backbone.View.extend({
+  tagName: 'ul',
+
+  render: function(){
+      var self = this;
+      this.collection.each(function(zips){
+        self.$el.append('<li>' + zips.get('body') + '</li>');
+      });
+    }
+  });
 
 
 ////////////////////////////////
@@ -45,7 +55,7 @@ var MyRouter = Backbone.Router.extend({
   },
 
   initialize: function() {
-    this.zips = new ZipsCollection([{body: 'First Data'}]);
+    this.zips = new ZipCollection([{body: 'First Data'}]);
     this.inputView = new ZipInputView({collection: this.zips});
     this.listView = new ZipsListView({collection: this.zips});
   },
@@ -71,94 +81,3 @@ $(document).ready(function() {
   Backbone.history.start();
 });
 /////////////////////////////////////////////////////////
-    //
-    //
-    //
-    // (function(){
-    //
-    //   var Toot = Backbone.Model.extend({
-    //     defaults: {
-    //       body: ""
-    //     }
-    //   });
-    //
-    //   var TootsCollection = Backbone.Collection.extend({
-    //     model: Toot
-    //   });
-
-
-
-
-
-
-
-    //   var TootInputView = Backbone.View.extend({
-    //     tagName: 'form',
-    //     events: {
-    //       'submit': 'createToot'
-    //     },
-    //
-    //     createToot: function(e){
-    //       e.preventDefault();
-    //       console.log('toot');
-    //     },
-    //
-    //     template: _.template( $('#toot-input-template').text() ),
-    //
-    //     render: function(){
-    //       this.$el.html( this.template() );
-    //       return this;
-    //     }
-    //   });
-
-
-
-
-
-
-    //   var TootsListView = Backbone.View.extend({
-    //     tagName: 'ul',
-    //
-    //     render: function(){
-    //       var self = this;
-    //       this.collection.each(function(toot){
-    //         self.$el.append('<li>' + toot.get('body') + '</li>');
-    //       });
-    //     }
-    //   });
-
-
-
-
-
-
-    //   var TooterRouter = Backbone.Router.extend({
-    //     routes: {
-    //       '': 'index',
-    //       'toot/:id': 'showToot'
-    //     },
-    //
-    //     initialize: function(){
-    //       this.toots = new TootsCollection([{body: 'First Toot'}]);
-    //       this.inputView = new TootInputView({collection: this.toots});
-    //       this.listView = new TootsListView({collection: this.toots});
-    //     },
-    //
-    //     index: function(){
-    //       this.inputView.render();
-    //       $('#app').append(this.inputView.el);
-    //       this.listView.render();
-    //       $('#app').append(this.listView.el);
-    //     },
-    //
-    //     showToot: function(id){
-    //       console.log(id);
-    //     }
-    //   });
-    //
-    //   $(document).ready(function(){
-    //     var router = new TooterRouter();
-    //     Backbone.history.start();
-    //   });
-    //
-    // })();
