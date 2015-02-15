a70ff1a287f42899fa886668a8a5c55dfe004330
Dustin Morando <dustinmorando@me.com>
2015-02-15 14:42:31 -0500

    hacking on jakes code

--- scripts/main.js
+++ scripts/main.js
@@ -1,90 +1,191 @@
+// ////////////////////////////////
+// ///Model
+// ////////////////////////////////
+// var Concert = Backbone.Model.extend({
+//   defaults: {
+//     body: ''
+//   }
+// });
+//
+// var ConcertCollection = Backbone.Collection.extend({
+//
+// });
+//
+//
+// ////////////////////////////////
+// ///View
+// ////////////////////////////////
+// var ZipInputView = Backbone.View.extend({
+//   tagName: 'form',
+//   events: {
+//     'submit': 'createData'
+//   },
+//
+//   createData: function(event) {
+//     event.preventDefault();
+//     console.log('data');
+//   },
+//
+//   template: _.template($('#index-template').text()),
+//
+//   render: function() {
+//     this.$el.html(this.template());
+//     return this;
+//   }
+// });
+//
+// var ZipsListView = Backbone.View.extend({
+//   tagName: 'ul',
+//
+//   render: function() {
+//     var self = this;
+//     this.collection.each(function(zips) {
+//       self.$el.append('<li>' + zips.get('body') + '</li>');
+//     });
+//   }
+// });
+//
+//
+// ////////////////////////////////
+// ///Router
+// ////////////////////////////////
+// var MyRouter = Backbone.Router.extend({
+//   routes: {
+//     "": "index",
+//     "results/:id": "showResults"
+//   },
+//
+//   initialize: function() {
+//     this.zips = new ZipCollection([{
+//       body: 'First Data'
+//     }]);
+//     this.inputView = new ZipInputView({
+//       collection: this.zips
+//     });
+//     this.listView = new ZipsListView({
+//       collection: this.zips
+//     });
+//   },
+//
+//
+//   index: function() {
+//     this.inputView.render();
+//     $('#app-container').append(this.inputView.el);
+//     this.listView.render();
+//     $('#app-container').append(this.listView.el);
+//   },
+//
+//   // results: function(id) {
+//   //   var template = _.template($('#results-template').text());
+//   //   var renderedTemplate = template("results-template");
+//   //   $('.app-container').html(renderedTemplate);
+//   // },
+//
+// });
+//
+// $(document).ready(function() {
+//   var router = new MyRouter();
+//   Backbone.history.start();
+// });
+// /////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
 ////////////////////////////////
 ///Model
 ////////////////////////////////
-var Zip = Backbone.Model.extend({
-  defaults: {
-    body: ''
-  }
-});
+window.App = window.App || {};
 
-var ZipCollection = Backbone.Collection.extend({
-  model: Zip
-});
+ var AppModel = Backbone.Model.extend({
+   defaults: {
+     searchTerm: ''
+   }
+ });
+
+ var Concert = Backbone.Model.extend({});
+
+ var ConcertCollection = Backbone.Collection.extend({
+   initialize: function(collection, options){
+     this.appModel = options.appModel;
+   },
+
+   url: function (){
+     var searchTerm = this.appModel.get('events');
+     var base = "http://api.bandsintown.com/artists/Skrillex/events.json?app_id=";
+     var appId = "something_music";
+      return base + appId + (searchTerm ? "venue" + searchTerm : "");
+
+     // return base + appId;
+
+   },
+
+   model: Concert,
+
+   sync: function(method, collection, options) {
+     options.dataType = 'jsonp';
+     Backbone.sync(method, collection, options);
+   },
+
+   parse: function(response) {
+     return response.results;
+   }
+ });
 
 
 ////////////////////////////////
 ///View
 ////////////////////////////////
-var ZipInputView = Backbone.View.extend({
-  tagName: 'form',
-  events: {
-    'submit': 'createData'
-  },
+ var ListView = Backbone.View.extend({
 
-  createData: function(event) {
-    event.preventDefault();
-    console.log('data');
-  },
+   el: '#events',
 
-  template: _.template($('#index-template').text()),
+   initialize: function(){
+     this.listenTo(this.collection, 'sync', this.render);
+   },
 
-  render: function() {
-    this.$el.html(this.template());
-    return this;
-  }
-});
-
-var ZipsListView = Backbone.View.extend({
-  tagName: 'ul',
-
-  render: function() {
-    var self = this;
-    this.collection.each(function(zips) {
-      self.$el.append('<li>' + zips.get('body') + '</li>');
-    });
-  }
-});
+   render: function(){
+     this.$el.empty();
+     var self = this;
+     this.collection.each(function(event){
+       self.$el.append('<li>' + event.get('venue') + '</li>');
+     });
+   },
+ });
 
 
 ////////////////////////////////
 ///Router
 ////////////////////////////////
-var MyRouter = Backbone.Router.extend({
-  routes: {
-    "": "index",
-    "results/:id": "showResults"
-  },
+ var AppRouter = Backbone.Router.extend({
+   routes: {
+     '': 'index',
+     'search/:term': 'search'
+   },
 
-  initialize: function() {
-    this.zips = new ZipCollection([{
-      body: 'First Data'
-    }]);
-    this.inputView = new ZipInputView({
-      collection: this.zips
-    });
-    this.listView = new ZipsListView({
-      collection: this.zips
-    });
-  },
+   initialize: function(){
+     this.appModel = new AppModel();
+     this.events = new ConcertCollection([], {appModel: this.appModel});
+     this.listView = new ListView({collection: this.events});
+     this.listView.render();
+   },
 
+   index: function(){
+     this.events.fetch();
+   },
 
-  index: function() {
-    this.inputView.render();
-    $('#app-container').append(this.inputView.el);
-    this.listView.render();
-    $('#app-container').append(this.listView.el);
-  },
+   search: function(term){
+     this.appModel.set('searchTerm', term);
+     this.events.fetch();
+   }
+ });
 
-  // results: function(id) {
-  //   var template = _.template($('#results-template').text());
-  //   var renderedTemplate = template("results-template");
-  //   $('.app-container').html(renderedTemplate);
-  // },
-
-});
-
-$(document).ready(function() {
-  var router = new MyRouter();
-  Backbone.history.start();
-});
-/////////////////////////////////////////////////////////
+ $(document).ready(function(){
+   window.router = new AppRouter();
+   Backbone.history.start();
+ });
